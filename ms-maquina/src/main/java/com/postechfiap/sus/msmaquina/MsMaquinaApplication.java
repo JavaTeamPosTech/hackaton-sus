d5bbcdc7762a285e15e3b5bdb5cd3894fc4762ac
Rafael Caxixi <rafael.fuzeti@sptech.school>
2026-01-25 20:53:30 -0300

    Inicializacao do ms maquina

---
+++ ms-maquina/src/main/java/com/postechfiap/sus/msmaquina/MsMaquinaApplication.java
@@ -0,0 +1,13 @@
+package com.postechfiap.sus.msmaquina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMaquinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMaquinaApplication.class, args);
+	}
+
+}
---
+++ ms-maquina/src/test/java/com/postechfiap/sus/msmaquina/MsMaquinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postechfiap.sus.msmaquina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMaquinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
